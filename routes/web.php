48e9dd3933ce4237c4288580328b26ebc686dac0
Jose alfredo <desarrollo@markas.com.bo>
2021-08-19 16:58:36 -0400

    Ruta sucursal añadida

--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,14 @@ use App\Http\Controllers\UploadFileIndependent;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [CreditController::class, 'index']);
-Route::get('solicit_credit_create', [CreditController::class, 'create'])->name('solicit_credit_create');
+Route::get('/sucursales', [CreditController::class, 'branch_offices'])->name('branch_offices');
+Route::get('/solicitar-credito', [CreditController::class, 'create'])->name('solicit_credit_create');
 Route::post('solicit_credit_store', [CreditController::class, 'store'])->name('solicit_credit_store');
 
-Route::get('upload_file_dependent', [UploadFileDependent::class, 'index'])->name('upload_file_dependent');
+Route::get('/cargar-archivo-dependiente', [UploadFileDependent::class, 'index'])->name('upload_file_dependent');
 Route::post('upload_file_dependent_store', [UploadFileDependent::class, 'store'])->name('upload_file_dependent_store');
 
-Route::get('upload_file_independent', [UploadFileIndependent::class, 'index'])->name('upload_file_independent');
+Route::get('/cargar-archivo-independiente', [UploadFileIndependent::class, 'index'])->name('upload_file_independent');
 Route::post('upload_file_independent_store', [UploadFileIndependent::class, 'store'])->name('upload_file_independent_store');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
