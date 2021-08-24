0cae1e6cc887eba197194da32a818df5f888fefb
Jose alfredo <desarrollo@markas.com.bo>
2021-08-24 15:16:59 -0400

    Mejora en los nuevos componentes

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<div class="relative py-8 bg-sky-900">
+<div class="py-8 bg-sky-900">
     <div class="mx-6">
         <p class="text-center text-sm text-white">© Copyright @php echo date('Y'); @endphp Hauscenter. Todos los derechos reservados.</p>
     </div>
