ae4ffa6dca3f88e81561ac5bf22db1027a176f5f
Jose alfredo <desarrollo@markas.com.bo>
2021-08-28 10:07:49 -0400

    Se añadio animaciones

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,4 +1,6 @@
-<div class="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0">
+<div data-aos="fade-right" data-aos-offset="200" data-aos-delay="50" data-aos-duration="500" data-aos-easing="ease-in-out"
+    data-aos-mirror="true" data-aos-once="false" data-aos-anchor-placement=".banner"
+    class="relative flex flex-col-reverse py-16 lg:pt-0 lg:flex-col lg:pb-0 banner bg-gray-200">
     <div
         class="inset-y-0 top-0 right-0 w-full max-w-xl px-4 mx-auto md:px-0 lg:pr-0 lg:mb-0 lg:mx-0 lg:w-7/12 lg:max-w-full lg:absolute xl:px-0">
         <svg class="absolute left-0 hidden h-full text-white transform -translate-x-1/2 lg:block" viewBox="0 0 100 100"
@@ -21,7 +23,8 @@
                 <span class="inline-block text-sky-800">crédito online</span>
             </h2>
             <p class="pr-5 mb-5 text-base text-gray-700 md:text-lg">
-                Ahora puedes realizar tu solicitud de crédito de forma más rápida y sencilla y lo mejor de todo sin salir de tu casa.
+                Ahora puedes realizar tu solicitud de crédito de forma más rápida y sencilla y lo mejor de todo sin
+                salir de tu casa.
             </p>
             <div class="flex items-start">
                 <a href="{{ route('solicit_credit_create') }}"
